a1569894b581b21209440a79aa967df38a1657ff
tingfer <tingfer@gmail.com>
2023-02-15 16:39:22 +0800

    function, Out-File, Start

--- Variables.ps1
+++ Variables.ps1
@@ -1,3 +1,11 @@
 ﻿#https://www.tutorialspoint.com/powershell/powershell_special_variables.htm
 
-Write-Host $Env:Path
+#Write-Host $Env:Path
+
+function EnvPathToFile()
+{
+    $Env:Path | Out-File -FilePath .\EnvPath.txt
+    Start .\EnvPath.txt
+}
+
+EnvPathToFile
